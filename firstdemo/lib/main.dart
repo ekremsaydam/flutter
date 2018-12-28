ea0257f3e233dcb1eed5b1ec22caed9c50db3435
ekremsaydam <30255407+ekremsaydam@users.noreply.github.com>
2018-12-28 14:54:01 +0300

    Custom Widget, StatelessWidget

--- firstdemo/lib/main.dart
+++ firstdemo/lib/main.dart
@@ -25,26 +25,54 @@ import 'package:flutter/material.dart';
 //   ));
 // }
 
-void main() {
-  runApp(MaterialApp(
-    title: "My First App",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("App Bar Title"),
-      ),
-      body: Material(
-        color: Colors.red,
-        child: Center(
-          child: Text(
-            "Hello World",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 50.0,
+// void main() {
+//   runApp(MaterialApp(
+//     title: "My First App",
+//     home: Scaffold(
+//       appBar: AppBar(
+//         title: Text("App Bar Title"),
+//       ),
+//       body: Material(
+//         color: Colors.red,
+//         child: Center(
+//           child: Text(
+//             "Hello World",
+//             textDirection: TextDirection.ltr,
+//             style: TextStyle(
+//               color: Colors.black,
+//               fontSize: 50.0,
+//             ),
+//           ),
+//         ),
+//       ),
+//     ),
+//   ));
+// }
+
+class HelloWorld extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My First Custom Widget",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First Custom Widget"),
+        ),
+        body: Material(
+          color: Colors.red,
+          child: Center(
+            child: Text(
+              "Hello World",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.black, fontSize: 50.0),
             ),
           ),
         ),
       ),
-    ),
-  ));
+    );
+  }
+}
+
+void main() {
+  runApp(HelloWorld());
 }
